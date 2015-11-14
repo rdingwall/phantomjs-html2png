fddeb9408aca983048c12123de28982f0e707815
Richard Dingwall <rdingwall@gmail.com>
2015-11-14 21:47:47 +0000

    Single instance of phantomjs

--- server.js
+++ server.js
@@ -17,28 +17,29 @@ app.get('/', function (req, res) {
 
 app.use(express.static('static'));
 
-app.post('/images', function(req, res) {
-    if (typeof req.body !== 'string' || req.body.length == 0) {
-        console.log('warning: ' + util.inspect(req.body));
-        res.status(400).send('bad request');
-    }
-    var bodyHtml = base64url.decode(req.body);
-    var id = uuid.v1();
-    var htmlFilename = process.env.HTML2PNG_TMP_DIR + '/' + id + '.html';
-    var pngFilename = process.env.HTML2PNG_STATIC_DIR + '/' + id + '.png';
-    var pngUri = process.env.HTML2PNG_URI + '/' + id + '.png';
+phantom.create(function (ph) {
 
-    console.log('id=' + id + ', html=' + htmlFilename + ', png=' + pngFilename);
-
-    fs.writeFile(htmlFilename, bodyHtml, function (err) {
-        if(err) {
-            console.log(err);
-            res.status(500).send(err);
+    app.post('/images', function(req, res) {
+        if (typeof req.body !== 'string' || req.body.length == 0) {
+            console.log('warning: ' + util.inspect(req.body));
+            res.status(400).send('bad request');
         }
+        var bodyHtml = base64url.decode(req.body);
+        var id = uuid.v1();
+        var htmlFilename = process.env.HTML2PNG_TMP_DIR + '/' + id + '.html';
+        var pngFilename = process.env.HTML2PNG_STATIC_DIR + '/' + id + '.png';
+        var pngUri = process.env.HTML2PNG_URI + '/' + id + '.png';
 
-        console.log('The file was saved!')
+        console.log('id=' + id + ', html=' + htmlFilename + ', png=' + pngFilename);
 
-        phantom.create(function (ph) {
+        fs.writeFile(htmlFilename, bodyHtml, function (err) {
+            if(err) {
+                console.log(err);
+                res.status(500).send(err);
+            }
+
+            console.log('The file was saved!')
+
             ph.createPage(function(page) {
                 page.viewportSize = { width: 1080, height: 1080 };
                 page.clipRect = { top: 0, left: 0, width: 1080, height: 1080 };
@@ -46,7 +47,7 @@ app.post('/images', function(req, res) {
                     console.log('page opened');
                     page.render(pngFilename);
                     console.log('file saved');
-                    res.status(200).send({uri: pngUri});
+                    res.status(200).send({uri: pngUri})
                 });
             });
         });
